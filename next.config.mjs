9b610983248b788fd78bdd2eaac6e7bee1086821
Gabriel Ferreira <gabriel_210203@hotmail.com>
2026-05-27 23:20:04 -0300

    Default vercel deploy config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    // Ignora os erros de ESLint na hora de subir para a Vercel
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // Ignora os errinhos de tipagem do TypeScript na hora do build
-    ignoreBuildErrors: true,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
